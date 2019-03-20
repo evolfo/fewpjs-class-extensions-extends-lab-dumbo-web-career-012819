d1cc751433dd6400a0577ef1dce49ab110e9b2cc
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-20 15:25:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get getCount() {
+    return this.sides.length
+  }
+
+  get perimeter() {
+    function getSum(total, num) {
+      return total + num;
+    }
+    return this.sides.reduce(getSum);
+  }
+
+}
+
+class Triangle extends Polygon {
+
+  get isValid() {
+    if (this.sides[0] + this.sides[1] > this.sides[2] && this.sides[0] + this.sides[2] > this.sides[1] && this.sides[1] + this.sides[2] > this.sides[0]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Square extends Polygon {
+
+  get isValid() {
+    if ((this.sides[0] === this.sides[1]) && (this.sides[2] === this.sides[3]) && (this.sides[0] === this.sides[2])) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get area() {
+    return this.sides[0] * this.sides[0];
+  }
+}
